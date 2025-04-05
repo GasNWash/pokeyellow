712f9e048b7127c1dce865630eab521bb16e9773
GasNWash <jahern2398@yahoo.com>
2025-04-05 12:23:14 -0500

    Script? Viridian Gym Jessie and James

--- scripts/ViridianGym.asm
+++ scripts/ViridianGym.asm
@@ -17,10 +17,20 @@ ViridianGym_Script:
 	db "GIOVANNI@"
 
 ViridianGymResetScripts:
+	CheckAndResetEvent EVENT_052
+	call nz, ViridianGymScript_HideJessieJames
 	xor a
 	ld [wJoyIgnore], a
+ViridianGymSetScript:
 	ld [wViridianGymCurScript], a
 	ld [wCurMapScript], a
+	ret
+
+ViridianGymScript_HideJessieJames:
+	ld a, HS_VIRIDIAN_GYM_JAMES 
+	call ViridianScript_HideObject
+	ld a, HS_VIRIDIAN_GYM_JESSIE
+	call ViridianGymScript_HideObject
 	ret
 
 ViridianGym_ScriptPointers:
@@ -29,6 +39,16 @@ ViridianGym_ScriptPointers:
 	dw_const DisplayEnemyTrainerTextAndStartBattle, SCRIPT_VIRIDIANGYM_START_BATTLE
 	dw_const EndTrainerBattle,                      SCRIPT_VIRIDIANGYM_END_BATTLE
 	dw_const ViridianGymGiovanniPostBattle,         SCRIPT_VIRIDIANGYM_GIOVANNI_POST_BATTLE
+	dw_const ViridianGymScript5,               SCRIPT_VIRIDIANGYM_SCRIPT5
+	dw_const ViridianGymScript6,               SCRIPT_VIRIDIANGYM_SCRIPT6
+	dw_const ViridianGymScript7,               SCRIPT_VIRIDIANGYM_SCRIPT7
+	dw_const ViridianGymScript8,               SCRIPT_VIRIDIANGYM_SCRIPT8
+	dw_const ViridianGymScript9,               SCRIPT_VIRIDIANGYM_SCRIPT9
+	dw_const ViridianGymScript10,               SCRIPT_VIRIDIANGYM_SCRIPT10
+	dw_const ViridianGymScript11,              SCRIPT_VIRIDIANGYM_SCRIPT11
+	dw_const ViridianGymScript12,              SCRIPT_VIRIDIANGYM_SCRIPT12
+	dw_const ViridianGymScript13,              SCRIPT_VIRIDIANGYM_SCRIPT13
+	dw_const ViridianGymScript14,              SCRIPT_VIRIDIANGYM_SCRIPT14
 	dw_const ViridianGymPlayerSpinningScript,       SCRIPT_VIRIDIANGYM_PLAYER_SPINNING
 
 ViridianGymDefaultScript:
@@ -39,6 +59,9 @@ ViridianGymDefaultScript:
 	ld hl, ViridianGymArrowTilePlayerMovement
 	call DecodeArrowMovementRLE
 	cp $ff
+	CheckEvent EVENT_BEAT_VIRIDIAN_GYM_JESSIE_JAMES
+	call z, ViridianGymScript_455a5
+	CheckEvent EVENT_BEAT_VIRIDIAN_GYM_TRAINER_7
 	jp z, CheckFightingMapTrainers
 	call StartSimulatingJoypadStates
 	ld hl, wd736
@@ -167,9 +190,219 @@ ViridianGymReceiveTM27:
 	SetEvents EVENT_2ND_ROUTE22_RIVAL_BATTLE, EVENT_ROUTE22_RIVAL_WANTS_BATTLE
 	jp ViridianGymResetScripts
 
+ViridianGymScript_455a5:
+	ld a, [wYCoord]
+	cp $e
+	ret nz
+	ResetEvent EVENT_BEAT_VIRIDIAN_GYM_JESSIE_JAMES_ON_LEFT
+	ld a, [wXCoord]
+	cp $18
+	jr z, .asm_455c2
+	ld a, [wXCoord]
+	cp $19
+	ret nz
+	SetEvent EVENT_BEAT_VIRIDIAN_GYM_JESSIE_JAMES_ON_LEFT
+.asm_455c2
+	xor a
+	ldh [hJoyHeld], a
+	ld a, SELECT | START | D_RIGHT | D_LEFT | D_UP | D_DOWN
+	ld [wJoyIgnore], a
+	call StopAllMusic
+	ld c, BANK(Music_MeetJessieJames)
+	ld a, MUSIC_MEET_JESSIE_JAMES
+	call PlayMusic
+	call UpdateSprites
+	call Delay3
+	call UpdateSprites
+	call Delay3
+	ld a, $1
+	ld [wDoNotWaitForButtonPressAfterDisplayingText], a
+	ld a, TEXT_VIRIDIANGYM_TEXT17  ;;;;;;;;;;;
+	ldh [hSpriteIndexOrTextID], a
+	call DisplayTextID
+	xor a
+	ld [wDoNotWaitForButtonPressAfterDisplayingText], a
+	ld a, A_BUTTON | B_BUTTON | SELECT | START | D_RIGHT | D_LEFT | D_UP | D_DOWN
+	ld [wJoyIgnore], a
+	ld a, HS_VIRIDIAN_GYM_JAMES 
+	call ViridianGymScript_ShowObject
+	ld a, HS_VIRIDIAN_GYM_JESSIE
+	call ViridianGymScript_ShowObject
+	ld a, SCRIPT_VIRIDIANGYM_SCRIPT5
+	call ViridianGymSetScript
+	ret
+
+ViridianGymJessieJamesMovementData_45605:
+	db $4
+ViridianGymJessieJamesMovementData_45606:
+	db $4
+	db $4
+	db $4
+	db $ff
+
+ViridianGymScript5:
+	ld de, ViridianGymJessieJamesMovementData_45605
+	CheckEvent EVENT_BEAT_VIRIDIAN_GYM_JESSIE_JAMES_ON_LEFT
+	jr z, .asm_45617
+	ld de, ViridianGymJessieJamesMovementData_45606
+.asm_45617
+	ld a, VIRIDIANGYM_JAMES
+	ldh [hSpriteIndexOrTextID], a
+	call MoveSprite
+	ld a, A_BUTTON | B_BUTTON | SELECT | START | D_RIGHT | D_LEFT | D_UP | D_DOWN
+	ld [wJoyIgnore], a
+	ld a, SCRIPT_VIRIDIANGYM_SCRIPT5
+	call ViridianGymSetScript
+	ret
+
+ViridianGymScript6:
+	ld a, A_BUTTON | B_BUTTON | SELECT | START | D_RIGHT | D_LEFT | D_UP | D_DOWN
+	ld [wJoyIgnore], a
+	ld a, [wd730]
+	bit 0, a
+	ret nz
+ViridianGymScript7:
+	ld a, $2
+	ld [wSprite02StateData1MovementStatus], a
+	ld a, SPRITE_FACING_LEFT
+	ld [wSprite02StateData1FacingDirection], a
+	CheckEvent EVENT_BEAT_VIRIDIAN_GYM_JESSIE_JAMES_ON_LEFT
+	jr z, .asm_4564a
+	ld a, SPRITE_FACING_DOWN
+	ld [wSprite02StateData1FacingDirection], a
+.asm_4564a
+	call Delay3
+	ld a, SELECT | START | D_RIGHT | D_LEFT | D_UP | D_DOWN
+	ld [wJoyIgnore], a
+ViridianGymScript8:
+	ld de, ViridianGymJessieJamesMovementData_45606
+	CheckEvent EVENT_BEAT_VIRIDIAN_GYM_JESSIE_JAMES_ON_LEFT
+	jr z, .asm_4565f
+	ld de, ViridianGymJessieJamesMovementData_45605
+.asm_4565f
+	ld a, VIRIDIANGYM_JESSIE
+	ldh [hSpriteIndexOrTextID], a
+	call MoveSprite
+	ld a, A_BUTTON | B_BUTTON | SELECT | START | D_RIGHT | D_LEFT | D_UP | D_DOWN
+	ld [wJoyIgnore], a
+	ld a, SCRIPT_VIRIDIANGYM_SCRIPT9
+	call ViridianGymSetScript
+	ret
+
+ViridianGymScript9:
+	ld a, A_BUTTON | B_BUTTON | SELECT | START | D_RIGHT | D_LEFT | D_UP | D_DOWN
+	ld [wJoyIgnore], a
+	ld a, [wd730]
+	bit 0, a
+	ret nz
+	ld a, SELECT | START | D_RIGHT | D_LEFT | D_UP | D_DOWN
+	ld [wJoyIgnore], a
+ViridianGymScript10:
+	ld a, $2
+	ld [wSprite03StateData1MovementStatus], a
+	ld a, SPRITE_FACING_DOWN
+	ld [wSprite03StateData1FacingDirection], a
+	CheckEvent EVENT_BEAT_VIRIDIAN_GYM_JESSIE_JAMES_ON_LEFT
+	jr z, .asm_45697
+	ld a, SPRITE_FACING_RIGHT
+	ld [wSprite03StateData1FacingDirection], a
+.asm_45697
+	call Delay3
+	ld a, TEXT_VIRIDIANGYM_TEXT18
+	ldh [hSpriteIndexOrTextID], a
+	call DisplayTextID
+ViridianGymScript11:
+	ld hl, wd72d
+	set 6, [hl]
+	set 7, [hl]
+	ld hl, ViridianGymJessieJamesEndBattleText
+	ld de, ViridianGymJessieJamesEndBattleText
+	call SaveEndBattleTextPointers
+	ld a, OPP_ROCKET
+	ld [wCurOpponent], a
+	ld a, $2e
+	ld [wTrainerNo], a
+	xor a
+	ldh [hJoyHeld], a
+	ld [wJoyIgnore], a
+	SetEvent EVENT_052
+	ld a, SCRIPT_VIRIDIANGYM_SCRIPT12
+	call ViridianGymSetScript
+	ret
+
+ViridianGymScript12:
+	ld a, A_BUTTON | B_BUTTON | SELECT | START | D_RIGHT | D_LEFT | D_UP | D_DOWN
+	ld [wJoyIgnore], a
+	ld a, [wIsInBattle]
+	cp $ff
+	jp z, ViridianGymResetScripts
+	ld a, $2
+	ld [wSprite02StateData1MovementStatus], a
+	ld [wSprite03StateData1MovementStatus], a
+	xor a
+	ld [wSprite02StateData1FacingDirection], a
+	ld [wSprite03StateData1FacingDirection], a
+	ld a, SELECT | START | D_RIGHT | D_LEFT | D_UP | D_DOWN
+	ld [wJoyIgnore], a
+	ld a, $1
+	ld [wDoNotWaitForButtonPressAfterDisplayingText], a
+	ld a, TEXT_VIRIDIANGYM_TEXT19
+	ldh [hSpriteIndexOrTextID], a
+	call DisplayTextID
+	xor a
+	ld [wDoNotWaitForButtonPressAfterDisplayingText], a
+	call StopAllMusic
+	ld c, BANK(Music_MeetJessieJames)
+	ld a, MUSIC_MEET_JESSIE_JAMES
+	call PlayMusic
+	ld a, A_BUTTON | B_BUTTON | SELECT | START | D_RIGHT | D_LEFT | D_UP | D_DOWN
+	ld [wJoyIgnore], a
+	ld a, SCRIPT_VIRIDIANGYM_SCRIPT13
+	call ViridianGymSetScript
+	ret
+
+ViridianGymScript13:
+	ld a, A_BUTTON | B_BUTTON | SELECT | START | D_RIGHT | D_LEFT | D_UP | D_DOWN
+	ld [wJoyIgnore], a
+	call GBFadeOutToBlack
+	ld a, HS_VIRIDIAN_GYM_JAMES  
+	call ViridianGymScript_HideObject
+	ld a, HS_VIRIDIAN_GYM_JESSIE
+	call ViridianGymScript_HideObject
+	call UpdateSprites
+	call Delay3
+	call GBFadeInFromBlack
+	ld a, SCRIPT_VIRIDIANGYM_SCRIPT14
+	call ViridianGymSetScript
+	ret
+
+ViridianGymScript14:
+	call PlayDefaultMusic
+	xor a
+	ldh [hJoyHeld], a
+	ld [wJoyIgnore], a
+	SetEvent EVENT_BEAT_VIRIDIAN_GYM_JESSIE_JAMES
+	ld a, SCRIPT_VIRIDIANGYM_DEFAULT
+	call ViridianGymSetScript
+	ret
+
+ViridianGymScript_ShowObject:
+	ld [wMissableObjectIndex], a
+	predef ShowObject
+	call UpdateSprites
+	call Delay3
+	ret
+
+ViridianGymScript_HideObject:
+	ld [wMissableObjectIndex], a
+	predef HideObject
+	ret
+
 ViridianGym_TextPointers:
 	def_text_pointers
 	dw_const ViridianGymGiovanniText,               TEXT_VIRIDIANGYM_GIOVANNI
+	dw_const ViridianGymJessieJamesText,            TEXT_VIRIDIANGYM_JAMES
+	dw_const ViridianGymJessieJamesText,            TEXT_VIRIDIANGYM_JESSIE
 	dw_const ViridianGymCooltrainerM1Text,          TEXT_VIRIDIANGYM_COOLTRAINER_M1
 	dw_const ViridianGymHiker1Text,                 TEXT_VIRIDIANGYM_HIKER1
 	dw_const ViridianGymRocker1Text,                TEXT_VIRIDIANGYM_ROCKER1
@@ -183,9 +416,12 @@ ViridianGym_TextPointers:
 	dw_const ViridianGymGiovanniEarthBadgeInfoText, TEXT_VIRIDIANGYM_GIOVANNI_EARTH_BADGE_INFO
 	dw_const ViridianGymGiovanniReceivedTM27Text,   TEXT_VIRIDIANGYM_GIOVANNI_RECEIVED_TM27
 	dw_const ViridianGymGiovanniTM27NoRoomText,     TEXT_VIRIDIANGYM_GIOVANNI_TM27_NO_ROOM
+	dw_const ViridianGymText17,                     TEXT_VIRIDIANGYM_TEXT17
+	dw_const ViridianGymText18,                TEXT_VIRIDIANGYM_TEXT18
+	dw_const ViridianGymText19,                TEXT_VIRIDIANGYM_TEXT19
 
 ViridianGymTrainerHeaders:
-	def_trainers 2
+	def_trainers 5
 ViridianGymTrainerHeader0:
 	trainer EVENT_BEAT_VIRIDIAN_GYM_TRAINER_0, 4, ViridianGymCooltrainerM1BattleText, ViridianGymCooltrainerM1EndBattleText, ViridianGymCooltrainerM1AfterBattleText
 ViridianGymTrainerHeader1:
@@ -203,6 +439,39 @@ ViridianGymTrainerHeader6:
 ViridianGymTrainerHeader7:
 	trainer EVENT_BEAT_VIRIDIAN_GYM_TRAINER_7, 4, ViridianGymCooltrainerM3BattleText, ViridianGymCooltrainerM3EndBattleText, ViridianGymCooltrainerM3AfterBattleText
 	db -1 ; end
+ViridianGymJessieJamesText:
+	text_end
+
+ViridianGymText17:
+	text_far _ViridianGymJessieJamesText1
+	text_asm
+	ld c, 10
+	call DelayFrames
+	ld a, $8
+	ld [wPlayerMovingDirection], a
+	ld a, $0
+	ld [wEmotionBubbleSpriteIndex], a
+	ld a, EXCLAMATION_BUBBLE
+	ld [wWhichEmotionBubble], a
+	predef EmotionBubble
+	ld c, 20
+	call DelayFrames
+	jp TextScriptEnd
+
+ViridianGymText18:
+	text_far _ViridianGymJessieJamesText2
+	text_end
+
+ViridianGymJessieJamesEndBattleText:
+	text_far _ViridianGymJessieJamesText3
+	text_end
+
+ViridianGymText19:
+	text_far _ViridianGymJessieJamesText4
+	text_asm
+	ld c, 64
+	call DelayFrames
+	jp TextScriptEnd
 
 ViridianGymGiovanniText:
 	text_asm
